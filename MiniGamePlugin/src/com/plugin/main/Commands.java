390f5975bba75f26f12aa162a2de19a55e8e3b0c
BuildTools <unconfigured@null.spigotmc.org>
2015-08-27 20:05:06 -0500

    Added the basics of the plugin.

---
+++ MiniGamePlugin/src/com/plugin/main/Commands.java
@@ -0,0 +1,17 @@
+package com.plugin.main;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Commands implements CommandExecutor {
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		Player player = (Player) sender;
+		
+		if (commandLabel.equalsIgnoreCase("test")) {
+			sender.sendMessage("This is working");
+		}
+		return false;
+	}
+}
---
+++ MiniGamePlugin/src/com/plugin/main/Main.java
@@ -0,0 +1,12 @@
+package com.plugin.main;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+}
